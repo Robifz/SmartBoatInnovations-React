eb8ed89eefd7c62248803ee3e5a93173709c63e5
Robifz <126383859+Robifz@users.noreply.github.com>
2023-06-10 08:17:18 +0200

    Commit from TeleportHQ

---
+++ src/views/about.js
@@ -0,0 +1,205 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Helmet } from 'react-helmet'
+
+import './about.css'
+
+const About = (props) => {
+  return (
+    <div className="about-container">
+      <Helmet>
+        <title>About - SmartBoatInnovations</title>
+        <meta
+          name="description"
+          content="Smart Boat ,  boat automation, IoT, intelligent monitoring, seamless connectivity, Home Assistant, Anchor Alarm, engine monitoring, bilge alarm, safety"
+        />
+        <meta property="og:title" content="About - SmartBoatInnovations" />
+        <meta
+          property="og:description"
+          content="Join me as we explore boat automation and the Internet of Things (IoT) technology, from intelligent monitoring to seamless connectivity.  Open systems and cost-effective designs."
+        />
+        <meta
+          property="og:image"
+          content="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/ea7f7557-3544-4a71-a4e9-4320e8b75036/0aa20519-76fc-42ec-9c9d-541129945274?org_if_sml=1"
+        />
+      </Helmet>
+      <div className="about-container1">
+        <div className="about-container2">
+          <header data-thq="thq-navbar" className="about-navbar-interactive">
+            <div
+              data-thq="thq-navbar-nav"
+              data-role="Nav"
+              className="about-desktop-menu"
+            >
+              <nav
+                data-thq="thq-navbar-nav-links"
+                data-role="Nav"
+                className="about-nav"
+              >
+                <Link to="/" className="about-navlink">
+                  Home
+                </Link>
+                <Link to="/about" className="about-navlink01">
+                  About
+                </Link>
+                <Link to="/services" className="about-navlink02">
+                  Services
+                </Link>
+                <Link to="/download-code" className="about-navlink03">
+                  Code
+                </Link>
+                <Link to="/contact" className="about-navlink04">
+                  Contact
+                </Link>
+              </nav>
+            </div>
+            <div
+              data-thq="thq-navbar-btn-group"
+              className="about-btn-group"
+            ></div>
+            <div data-thq="thq-burger-menu" className="about-burger-menu">
+              <svg viewBox="0 0 1024 1024" className="about-icon">
+                <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+              </svg>
+            </div>
+            <div data-thq="thq-mobile-menu" className="about-mobile-menu">
+              <div
+                data-thq="thq-mobile-menu-nav"
+                data-role="Nav"
+                className="about-nav1"
+              >
+                <div className="about-container3">
+                  <div
+                    data-thq="thq-close-menu"
+                    className="about-menu-close"
+                  ></div>
+                </div>
+                <nav
+                  data-thq="thq-mobile-menu-nav-links"
+                  data-role="Nav"
+                  className="about-nav2"
+                >
+                  <Link to="/" className="about-navlink05">
+                    Home
+                  </Link>
+                  <Link to="/about" className="about-navlink06">
+                    About
+                  </Link>
+                  <Link to="/services" className="about-navlink07">
+                    Services
+                  </Link>
+                  <Link to="/download-code" className="about-navlink08">
+                    Code
+                  </Link>
+                  <Link to="/contact" className="about-navlink09">
+                    Contact
+                  </Link>
+                </nav>
+                <div className="about-container4"></div>
+              </div>
+              <div className="about-icon-group">
+                <svg
+                  viewBox="0 0 950.8571428571428 1024"
+                  className="about-icon2"
+                >
+                  <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+                </svg>
+                <svg
+                  viewBox="0 0 877.7142857142857 1024"
+                  className="about-icon4"
+                >
+                  <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+                </svg>
+                <svg
+                  viewBox="0 0 602.2582857142856 1024"
+                  className="about-icon6"
+                >
+                  <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+                </svg>
+              </div>
+            </div>
+          </header>
+        </div>
+        <img alt="image" src="/wlan%20logo-300h.png" className="about-image" />
+        <div className="about-container5">
+          <span className="about-text">Smart Boat Innovations</span>
+        </div>
+      </div>
+      <h1 className="about-text1">About Me    </h1>
+      <span className="about-text2">
+        <span>
+          Welcome to Smart Boat Innovations! I&apos;m Rob, a seasoned sailor and
+          tech enthusiast dedicated to revolutionizing the boating industry
+          through the integration of accessible technology. With over 20 years
+          of global sailing experience and a Computer Science degree, I am
+          committed to providing you with practical innovations and ideas that
+          will transform your boating experience.
+        </span>
+        <br></br>
+        <br></br>
+        <span>
+          At Smart Boat Innovations, we explore the world of boat automation and
+          harness the power of the Internet of Things (IoT) technology. My
+          philosophy revolves around utilizing free open systems and affordable
+          sensors and equipment. I believe that everyone should have access to
+          these advancements without breaking the bank.While our website
+          provides valuable insights and information, the majority of our
+          content can be found on our YouTube channel. I invite you to head over
+          there, subscribe to our channel, and join the vibrant community of
+          boating enthusiasts who are exploring the exciting possibilities of
+          affordable technology.
+          <span
+            dangerouslySetInnerHTML={{
+              __html: ' ',
+            }}
+          />
+        </span>
+      </span>
+      <div className="about-cards-container"></div>
+      <footer className="about-footer">
+        <div className="about-container6">
+          <nav className="about-nav3">
+            <Link to="/" className="about-navlink10">
+              Home
+            </Link>
+            <Link to="/about" className="about-navlink11">
+              About
+            </Link>
+            <Link to="/services" className="about-navlink12">
+              Services
+            </Link>
+            <Link to="/download-code" className="about-navlink13">
+              Code
+            </Link>
+            <Link to="/contact" className="about-navlink14">
+              Contact
+            </Link>
+          </nav>
+        </div>
+        <div className="about-separator"></div>
+        <div className="about-container7">
+          <span className="about-text7">
+            © 2023 Smart Boat Innovations, All Rights Reserved.
+          </span>
+          <div className="about-icon-group1">
+            <a
+              href="https://ko-fi.com/smartboatinnovations"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="about-link"
+            >
+              <img
+                alt="image"
+                src="/kofi_logo_blue.svg"
+                className="about-image1"
+              />
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default About
---
+++ src/views/download-code.js
@@ -0,0 +1,217 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Helmet } from 'react-helmet'
+
+import './download-code.css'
+
+const DownloadCode = (props) => {
+  return (
+    <div className="download-code-container">
+      <Helmet>
+        <title>Download-Code - SmartBoatInnovations</title>
+        <meta
+          name="description"
+          content="Smart Boat ,  boat automation, IoT, intelligent monitoring, seamless connectivity, Home Assistant, Anchor Alarm, engine monitoring, bilge alarm, safety"
+        />
+        <meta
+          property="og:title"
+          content="Download-Code - SmartBoatInnovations"
+        />
+        <meta
+          property="og:description"
+          content="Join me as we explore boat automation and the Internet of Things (IoT) technology, from intelligent monitoring to seamless connectivity.  Open systems and cost-effective designs."
+        />
+        <meta
+          property="og:image"
+          content="https://aheioqhobo.cloudimg.io/v7/_playground-bucket-v2.teleporthq.io_/ea7f7557-3544-4a71-a4e9-4320e8b75036/0aa20519-76fc-42ec-9c9d-541129945274?org_if_sml=1"
+        />
+      </Helmet>
+      <div className="download-code-container1">
+        <div className="download-code-container2">
+          <header
+            data-thq="thq-navbar"
+            className="download-code-navbar-interactive"
+          >
+            <div
+              data-thq="thq-navbar-nav"
+              data-role="Nav"
+              className="download-code-desktop-menu"
+            >
+              <nav
+                data-thq="thq-navbar-nav-links"
+                data-role="Nav"
+                className="download-code-nav"
+              >
+                <Link to="/" className="download-code-navlink">
+                  Home
+                </Link>
+                <span className="download-code-text">About</span>
+                <Link to="/services" className="download-code-navlink01">
+                  Services
+                </Link>
+                <Link to="/download-code" className="download-code-navlink02">
+                  Code
+                </Link>
+                <Link to="/contact" className="download-code-navlink03">
+                  Contact
+                </Link>
+              </nav>
+            </div>
+            <div
+              data-thq="thq-navbar-btn-group"
+              className="download-code-btn-group"
+            ></div>
+            <div
+              data-thq="thq-burger-menu"
+              className="download-code-burger-menu"
+            >
+              <svg viewBox="0 0 1024 1024" className="download-code-icon">
+                <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+              </svg>
+            </div>
+            <div
+              data-thq="thq-mobile-menu"
+              className="download-code-mobile-menu"
+            >
+              <div
+                data-thq="thq-mobile-menu-nav"
+                data-role="Nav"
+                className="download-code-nav1"
+              >
+                <div className="download-code-container3">
+                  <div
+                    data-thq="thq-close-menu"
+                    className="download-code-menu-close"
+                  ></div>
+                </div>
+                <nav
+                  data-thq="thq-mobile-menu-nav-links"
+                  data-role="Nav"
+                  className="download-code-nav2"
+                >
+                  <Link to="/" className="download-code-navlink04">
+                    Home
+                  </Link>
+                  <Link to="/about" className="download-code-navlink05">
+                    About
+                  </Link>
+                  <Link to="/services" className="download-code-navlink06">
+                    Services
+                  </Link>
+                  <Link to="/download-code" className="download-code-navlink07">
+                    Code
+                  </Link>
+                  <Link to="/contact" className="download-code-navlink08">
+                    Contact
+                  </Link>
+                </nav>
+                <div className="download-code-container4"></div>
+              </div>
+              <div className="download-code-icon-group">
+                <svg
+                  viewBox="0 0 950.8571428571428 1024"
+                  className="download-code-icon2"
+                >
+                  <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+                </svg>
+                <svg
+                  viewBox="0 0 877.7142857142857 1024"
+                  className="download-code-icon4"
+                >
+                  <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+                </svg>
+                <svg
+                  viewBox="0 0 602.2582857142856 1024"
+                  className="download-code-icon6"
+                >
+                  <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+                </svg>
+              </div>
+            </div>
+          </header>
+        </div>
+        <img
+          alt="image"
+          src="/wlan%20logo-300h.png"
+          className="download-code-image"
+        />
+        <div className="download-code-container5">
+          <span className="download-code-text01">Smart Boat Innovations</span>
+        </div>
+      </div>
+      <h1 className="download-code-text02">Download Code Centre</h1>
+      <span className="download-code-text03">
+        <span>
+          Welcome to the Smart Boat Innovations Download Center! I&apos;m here
+          to share a collection of code snippets that I&apos;ve personally
+          crafted to enhance your smart boat&apos;s capabilities. I&apos;ve put
+          in a lot of time and effort to develop this code, and if you find it
+          valuable, I kindly ask for your support through a donation. Your
+          contribution helps me fuel my passion for advancing boating technology
+          and enables future projects.
+        </span>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+      </span>
+      <a
+        href="https://ko-fi.com/smartboatinnovations"
+        target="_blank"
+        rel="noreferrer noopener"
+        className="download-code-link"
+      >
+        <img
+          alt="image"
+          src="/kofi_logo_blue.svg"
+          className="download-code-image1"
+        />
+      </a>
+      <div className="download-code-cards-container"></div>
+      <footer className="download-code-footer">
+        <div className="download-code-container6">
+          <nav className="download-code-nav3">
+            <Link to="/" className="download-code-navlink09">
+              Home
+            </Link>
+            <Link to="/about" className="download-code-navlink10">
+              About
+            </Link>
+            <Link to="/services" className="download-code-navlink11">
+              Services
+            </Link>
+            <Link to="/download-code" className="download-code-navlink12">
+              Code
+            </Link>
+            <Link to="/contact" className="download-code-navlink13">
+              Contact
+            </Link>
+          </nav>
+        </div>
+        <div className="download-code-separator"></div>
+        <div className="download-code-container7">
+          <span className="download-code-text09">
+            © 2023 Smart Boat Innovations, All Rights Reserved.
+          </span>
+          <div className="download-code-icon-group1">
+            <a
+              href="https://ko-fi.com/smartboatinnovations"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="download-code-link1"
+            >
+              <img
+                alt="image"
+                src="/kofi_logo_blue.svg"
+                className="download-code-image2"
+              />
+            </a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default DownloadCode
